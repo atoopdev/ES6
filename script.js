e7f6c9b873325c3f2199a872539ab7647d65edcd
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-05-04 13:11:35 -0700

    object deconstruction

--- script.js
+++ script.js
@@ -32,12 +32,86 @@ const planet = "Earth"
 // console.log(first)
 
 // searching arrays with .includes
-let cities = [
-    "Spokane",
-    "Boston",
-    "Los Angeles",
-    "Seattle",
-    "Portland"
-]
-// returns true
-console.log(cities.includes("Boston"))
+// let cities = [
+//     "Spokane",
+//     "Boston",
+//     "Los Angeles",
+//     "Seattle",
+//     "Portland"
+// ]
+// // returns true
+// console.log(cities.includes("Boston"))
+
+// objects
+// use spread operator "..." to prevent nested objects
+// const daytimeMeals= {
+//     breakfast: "oatmeal",
+//     lunch: "pbj"
+// };
+
+// const nighttimeMeals = "mac and cheese";
+
+// const dayofMeals = {
+//     ...daytimeMeals,
+//     nighttimeMeals
+// }
+
+// console.log(dayofMeals)
+
+// destructuring objects
+// before
+// const sandwich = {
+//     title: "Turkey",
+//     price: 7,
+//     description: "A classic",
+//     ingredients: [
+//         "bread",
+//         "turkey",
+//         "lettuce",
+//         "tomato",
+//         "mayo"
+//     ]
+// }
+
+// console.log(sandwich)
+
+// after
+// const {title, price} = {
+//     title: "Turkey",
+//     price: 7,
+//     description: "A classic",
+//     ingredients: [
+//         "bread",
+//         "turkey",
+//         "lettuce",
+//         "tomato",
+//         "mayo"
+//     ]
+// }
+
+// console.log(title)
+
+// another example
+// const vacation = {
+//     destination: "Hawaii",
+//     travelers: 4,
+//     activity: "swimming",
+//     cost: 4000
+// };
+
+// function marketing({destination, activity}){
+//     return `Come to ${destination} and do some ${activity}`
+// }
+
+// console.log(marketing(vacation))
+
+// for/of loop
+// for(let letter of "santa"){
+//     console.log(letter)
+// }
+
+// another example
+let topics = ["Baking", "Stir fries", "Pickles"]
+for(let topic of topics){
+    console.log(topic)
+}
